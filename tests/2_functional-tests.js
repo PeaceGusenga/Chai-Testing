e3224315b669cb0788df41116c0d981c8cd39807
Peace_iMAC <gusengapeace@gmail.com>
2024-07-29 07:25:32 +0200

    headless browser setup

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -67,9 +67,15 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://0.0.0.0:3000';
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+  const browser = new Browser();
+
+  suiteSetup(function(done){
+    return browser.visit('/',done);
+  })
 
 
 
